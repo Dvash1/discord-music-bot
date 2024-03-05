183b7cb8b64a47e2a789c1aefd9b88939bb7f347
Daniel Vash <67289040+Dvash1@users.noreply.github.com>
2024-03-05 16:13:32 +0200

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,1 +1,3 @@
-BOTTOKEN= '' # Enter your bot's token here!
+BOTTOKEN= 'EXAMPLEeXampleExaMple' # Enter your bot's token here!
+LAVALINK_IP = 'https://localhost:3000' # Enter your server's IP here
+LAVALINK_PASSWORD = '' # Enter your password here
